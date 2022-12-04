d677ab0d035ebf55a297be12d719e3393007914b
kk412027247 <geqhxds1234>
2022-12-04 23:58:56 +0800

    update

--- housing.py
+++ housing.py
@@ -94,3 +94,11 @@ plt.show()
 
 housing.plot(kind='scatter', x='median_income', y='median_house_value', alpha=0.1)
 plt.show()
+
+housing['rooms_per_household'] = housing['total_rooms'] / housing['households']
+housing['bedrooms_per_room'] = housing['total_bedrooms'] / housing['total_rooms']
+housing['population_per_household'] = housing['population'] / housing['households']
+
+corr_matrix = housing.corr()
+r = corr_matrix['median_house_value'].sort_values(ascending=False)
+print(r)
