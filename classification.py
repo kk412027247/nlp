27658874a4841a9969f14e89b788da44d2a24eb2
kk412027247 <geqhxds1234>
2022-12-15 20:09:12 +0800

    trade off

--- classification.py
+++ classification.py
@@ -2,6 +2,11 @@ from sklearn.datasets import fetch_openml
 from datetime import datetime
 from joblib import Memory
 import matplotlib.pyplot as plt
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.dummy import DummyClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_score, recall_score, f1_score, precision_recall_curve
 
 memory = Memory('./tmp')
 fetch_openml_cached = memory.cache(fetch_openml)
@@ -22,8 +27,88 @@ def plot_digit(image_data):
 
 some_digit = X[0]
 plot_digit(some_digit)
-plt.show()
+# plt.show()
 
 print(y[0])
 
-X_train, X_test, y_train, y_test = X[:6000], X[6000:], y[:6000], y[6000:]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+y_train_5 = (y_train == '5')
+y_test_5 = (y_test == '5')
+
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+predict_r = sgd_clf.predict([some_digit])
+print(predict_r)
+
+cross_val_score_r = cross_val_score(
+    sgd_clf, X_train, y_train_5, cv=3, scoring='accuracy')
+print(cross_val_score_r)
+
+# dummy_clf = DummyClassifier()
+# dummy_clf.fit(X_train, y_train_5)
+# print(any(dummy_clf.predict(X_train)))
+# cross_val_score_r = cross_val_score(dummy_clf, X_train, y_train_5, cv=3, scoring='accuracy')
+# print(cross_val_score_r)
+
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+cm = confusion_matrix(y_train_5, y_train_pred)
+print(cm)
+
+r = precision_score(y_train_5, y_train_pred)
+print(r)
+
+r = recall_score(y_train_5, y_train_pred)
+print(r)
+
+r = f1_score(y_train_5, y_train_pred)
+print(r)
+
+y_scores = sgd_clf.decision_function([some_digit])
+print(y_scores)
+
+threshold = 0
+y_some_digit_pred = (y_scores > threshold)
+print("y_some_digit_pred", y_some_digit_pred)
+
+threshold = 3000
+y_some_digit_pred = (y_scores > threshold)
+print("y_some_digit_pred", y_some_digit_pred)
+
+y_scores = cross_val_predict(
+    sgd_clf, X_train, y_train_5, cv=3, method='decision_function')
+
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+
+plt.figure(figsize=(8, 4))
+plt.plot(thresholds, precisions[:-1], "b--", label="Precision", linewidth=2)
+plt.plot(thresholds, recalls[:-1], "g-", label="Recall", linewidth=2)
+plt.vlines(threshold, 0, 1.0, "k", "dotted", label="threshold")
+plt.axis([-50000, 50000, 0, 1])
+plt.grid()
+plt.xlabel("Threshold")
+plt.legend(loc="center right")
+# plt.show()
+
+
+plt.figure(figsize=(6, 5))
+plt.plot(recalls, precisions, linewidth=2, label='Precision/Recall curve')
+plt.xlabel("Recall")
+plt.ylabel("Precision")
+plt.axis([0, 1, 0, 1])
+plt.grid()
+plt.legend(loc="lower left")
+# plt.show()
+
+idx_for_90_precision = (precisions >= 0.90).argmax()
+
+threshold_for_90_precision = thresholds[idx_for_90_precision]
+
+y_train_pred_90 = (y_scores >= threshold_for_90_precision)
+# return [False False False ...  True False False]
+
+r = precision_score(y_train_5, y_train_pred_90)
+print('precision_score', r)
+
+recall_at_90_precision = recall_score(y_train_5, y_train_pred_90)
+print('recall_at_90_precision', recall_at_90_precision)
